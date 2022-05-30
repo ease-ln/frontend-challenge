f1116f471da4cf60e6ed11149483a3ca560e90a9
ease-ln <arina031120000@gmail.com>
2022-05-30 06:32:57 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
-git add -Agit commit -m 'deploy'
+git add -A
+git commit -m 'deploy'
 git push -f git@github.com:ease-ln/frontend-challenge.git master:gh-pages
 cd -
